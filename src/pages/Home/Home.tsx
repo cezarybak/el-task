9c6b64a2c5f65ad1b006763b3f5ec9d17d818466
cezarybak <cezary.bak0@gmail.com>
2022-04-18 02:47:46 +0200

    feat: add spiner

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,27 +4,46 @@ import Navbar from "../../components/Navbar";
 import { SearchList } from "../../graphql/operations/queries";
 import { SearchListData, SearchListVars } from "../../types/SearchList";
 
+import "./style.scss";
+
 export const Home = () => {
   const navigate = useNavigate();
   const { data, loading } = useQuery<SearchListData, SearchListVars>(
     SearchList,
     {
-      variables: { queryString: "as", first: 5, type: "USER" },
+      variables: { queryString: "as", first: 50, type: "USER" },
     }
   );
+
+  const count = Intl.NumberFormat().format(data?.search.userCount || 0);
 
   return (
     <section>
       <Navbar />
-      <span>
-        {loading && <div>Loading...</div>}
-        <span>{data?.search.userCount}</span>
-        {data?.search.edges.map(({ node }) => (
-          <div onClick={() => navigate(`/user/${node.login}`)} key={node.id}>
-            {node.login}
-          </div>
-        ))}
-      </span>
+      <main className="home__container">
+        <div className="home__wraper">
+          {loading ? (
+            <div className="home_loader" />
+          ) : (
+            <div>
+              <span>{count} results</span>
+              {data?.search.edges.map(({ node }) => (
+                <div
+                  className="home_element"
+                  onClick={() => navigate(`/user/${node.login}`)}
+                  key={node.id}
+                >
+                  {node.login}
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      </main>
+      <div className="home_pagination">
+        <div className="home__paggination_button_disabled">Previous</div>
+        <div>Next</div>
+      </div>
     </section>
   );
 };
--- src/pages/User/User.tsx
+++ src/pages/User/User.tsx
@@ -1,6 +1,6 @@
 import Navbar from "../../components/Navbar";
-import StarIcon from "./../../assets/star.svg";
-import PersonsIcon from "./../../assets/person.svg";
+import StarIcon from "./../../assets/svg/star.svg";
+import PersonsIcon from "./../../assets/svg/person.svg";
 
 import "./style.scss";
 
@@ -8,23 +8,23 @@ export const User = () => {
   return (
     <section>
       <Navbar />
-      <main className="main">
+      <main className="user_container">
         <img src="" alt="" />
 
-        <div className="contatiner">
+        <div className="user__section">
           <div>
             <h1>Michał Maliszewski</h1>
             <h3>michał</h3>
           </div>
 
-          <div className="wraper">
-            <span>
-              <PersonsIcon className="icon" />
+          <div className="user__wraper">
+            <span className="user__span">
+              <PersonsIcon className="user__icon" />
               1221f Followers
             </span>
-            <span>2112 Following</span>
-            <span>
-              <StarIcon className="icon" />
+            <span className="user__span">2112 Following</span>
+            <span className="user__span">
+              <StarIcon className="user__icon" />
               12 Star
             </span>
           </div>
